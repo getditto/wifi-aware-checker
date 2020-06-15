217ebb35e5e00e405fd22f448b1e73333b4a73be
Max Alexander <max@ditto.live>
2020-06-15 16:04:02 -0700

    adding new resources and information

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This app quickly checks if your Android device supports WiFi Aware. Learn more a
 
 ## Why is this app useful?
 
-WiFi Aware is similar to Apple Wireless Direct, which is famously used in AirDrop. WiFi aware is introduced in the Android 8.0 (API level 26) or higher operating systems. However, it's up to the manufacturer to enable WiFi Aware at the firmware level.
+WiFi Aware is similar to Apple Wireless Direct, which is famously used in AirDrop. WiFi aware is introduced in the Android 8.0 (API level 26) or higher operating systems. _However, it's up to the manufacturer to enable WiFi Aware at the firmware level._
 
 _This means that you may have a device like a OnePlus 5T running Android 10 but WiFi Aware is still unavailable_.
 
@@ -13,4 +13,4 @@ Run this app and quickly find out if WiFi Aware is available on your device.
 
 WiFi Aware is an upcoming supported transport at [Ditto](https://www.ditto.live)
 
-![WiFi Aware Checker](https://media.giphy.com/media/KZYlo0pSCUEsohrTtC/giphy.gif)
+![WiFi Aware Checker](https://media.giphy.com/media/kfXxeX3zidS1yzKVgC/giphy.gif)
--- app/src/main/java/com/example/wifiawarechecker/MainActivity.kt
+++ app/src/main/java/com/example/wifiawarechecker/MainActivity.kt
@@ -1,26 +1,76 @@
 package com.example.wifiawarechecker
 
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
+import android.content.Intent.ACTION_VIEW
 import android.content.pm.PackageManager
-import android.net.wifi.aware.WifiAwareManager
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val hasFeatureTextView = findViewById<TextView>(R.id.has_feature_text_view)
+
+        val hasFeatureTextView: TextView = findViewById(R.id.has_feature_text_view)
+        val iconImageView: ImageView = findViewById(R.id.icon_image_view)
+        val modelAndManufacturerTextView: TextView =
+            findViewById(R.id.model_and_manufacturer_text_view)
+        val androidVersionTextView: TextView = findViewById(R.id.android_version_text_view);
+
+        modelAndManufacturerTextView.text = getDeviceName()
+        val version = Build.VERSION.SDK_INT
+        val versionRelease = Build.VERSION.RELEASE
+        androidVersionTextView.text = "Android Version: $version | Version Release $versionRelease"
+
+
+        val learnMoreButton: Button = findViewById(R.id.learn_more_button);
+        learnMoreButton.setOnClickListener {
+            val uri: Uri = Uri.parse("https://developer.android.com/guide/topics/connectivity/wifi-aware")
+            val browserIntent = Intent(ACTION_VIEW)
+            browserIntent.data = uri;
+            ContextCompat.startActivity(this, browserIntent, null)
+        }
+
         val hasSystemFeature = packageManager.hasSystemFeature(PackageManager.FEATURE_WIFI_AWARE)
         if (hasSystemFeature) {
             hasFeatureTextView.text = "WiFi Aware Available"
+            hasFeatureTextView.setTextColor(ContextCompat.getColor(this, R.color.colorAvailable));
+            iconImageView.setImageDrawable(getDrawable(R.drawable.ic_wifi_yes))
         } else {
             hasFeatureTextView.text = "WiFi Aware Unavailable"
+            hasFeatureTextView.setTextColor(ContextCompat.getColor(this, R.color.colorUnavailable));
+            iconImageView.setImageDrawable(getDrawable(R.drawable.ic_wifi_no))
+        }
+    }
+
+    private fun getDeviceName(): String? {
+        val manufacturer = Build.MANUFACTURER
+        val model = Build.MODEL
+        return if (model.startsWith(manufacturer)) {
+            capitalize(model)
+        } else {
+            capitalize(manufacturer) + " " + model
+        }
+    }
+
+
+    private fun capitalize(s: String?): String {
+        if (s == null || s.isEmpty()) {
+            return ""
+        }
+        val first = s[0]
+        return if (Character.isUpperCase(first)) {
+            s
+        } else {
+            Character.toUpperCase(first).toString() + s.substring(1)
         }
     }
 }
